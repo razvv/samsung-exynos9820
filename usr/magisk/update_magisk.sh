1c97e270ffa7de60de106f46a947532d56c0acb0
Denis Efremov <efremov@linux.com>
2021-01-26 01:22:37 +0300

    test
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- usr/magisk/update_magisk.sh
+++ usr/magisk/update_magisk.sh
@@ -9,7 +9,7 @@ ver="$(cat "$DIR/magisk_version" 2>/dev/null || echo -n 'none')"
 if [ "x$1" = "xcanary" ]
 then
 	nver="canary"
-	magisk_link="https://github.com/topjohnwu/magisk_files/raw/${nver}/magisk-debug.zip"
+	magisk_link="https://github.com/topjohnwu/magisk_files/raw/${nver}/app-debug.apk"
 else
 	if [ "x$1" = "x" ]; then
 		nver="$(curl -s https://github.com/topjohnwu/Magisk/releases | fgrep -m 1 'Magisk v' | cut -d '>' -f 2 | cut -d '<' -f 1 | cut -d ' ' -f 2)"
@@ -23,7 +23,8 @@ if [ \( -n "$nver" \) -a \( "$nver" != "$ver" \) -o ! \( -f "$DIR/arm/magiskinit
 then
 	echo "Updating Magisk from $ver to $nver"
 	curl -s --output "$DIR/magisk.zip" -L "$magisk_link"
-	unzip -o "$DIR/magisk.zip" arm/magiskinit64 -d "$DIR"
+	unzip -o "$DIR/magisk.zip" arm/magiskinit64 -d "$DIR" || unzip -o "$DIR/magisk.zip" lib/armeabi-v7a/libmagiskinit.so -d "$DIR"
+	mv -f "$DIR/arm/magiskinit64" "$DIR/magiskinit" || mv -f "$DIR/lib/armeabi-v7a/libmagiskinit.so" "$DIR/magiskinit"
 	echo -n "$nver" > "$DIR/magisk_version"
 	rm "$DIR/magisk.zip"
 	touch "$DIR/initramfs_list"
